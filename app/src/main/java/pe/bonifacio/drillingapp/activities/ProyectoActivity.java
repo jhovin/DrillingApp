262d4af8fd9071280ca6bbc6d75a10c2d6946113
Jhovin <jhovin.bonifacio@tecsup.edu.pe>
2020-02-19 16:54:35 -0500

    veinticuatroavo commit

--- app/src/main/java/pe/bonifacio/drillingapp/activities/ProyectoActivity.java
+++ app/src/main/java/pe/bonifacio/drillingapp/activities/ProyectoActivity.java
@@ -32,9 +32,6 @@ public class ProyectoActivity extends AppCompatActivity {
     private int mYearIni, mMonthIni, mDayIni, sYearIni, sMonthIni, sDayIni;
     static final int DATE_ID = 0;
     Calendar C = Calendar.getInstance();
-
-
-    private EditText etFechaFin;
     private EditText etDistrito;
     private EditText etProvincia;
     private EditText etDepartamento;
@@ -52,21 +49,9 @@ public class ProyectoActivity extends AppCompatActivity {
         sDayIni = C.get(Calendar.DAY_OF_MONTH);
         sYearIni = C.get(Calendar.YEAR);
 
-        etFechaInicio=findViewById(R.id.etFecha_inicio);
+        etFechaInicio = findViewById(R.id.etFecha_inicio);
         int textLength = etFechaInicio.getText().length();
-        etFechaInicio.setSelection(textLength,textLength);
-
-        etFechaFin=findViewById(R.id.etFecha_fin);
-        int editLength=etFechaFin.getText().length();
-        etFechaFin.setSelection(editLength,editLength);
-        etFechaFin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog(DATE_ID);
-            }
-        });
-
-
+        etFechaInicio.setSelection(textLength, textLength);
         etFechaInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,14 +62,14 @@ public class ProyectoActivity extends AppCompatActivity {
         setUpView();
 
     }
+
     public void setUpView() {
         usuario = SharedPrefManager.getInstance(getApplicationContext()).getUsuario();
         etProyecto = findViewById(R.id.etProyecto);
-        etFechaInicio=findViewById(R.id.etFecha_inicio);
-        etFechaFin=findViewById(R.id.etFecha_fin);
-        etDistrito=findViewById(R.id.etDistrito);
-        etProvincia=findViewById(R.id.etProvincia);
-        etDepartamento=findViewById(R.id.etDepartamento);
+        etFechaInicio = findViewById(R.id.etFecha_inicio);
+        etDistrito = findViewById(R.id.etDistrito);
+        etProvincia = findViewById(R.id.etProvincia);
+        etDepartamento = findViewById(R.id.etDepartamento);
         btCrearProyecto = findViewById(R.id.btCrearProyecto);
         btCrearProyecto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,10 +83,10 @@ public class ProyectoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 verTodosLosProyectos();
-                startActivity(new Intent(getApplicationContext(),ListaProyectosActivity.class));
+                startActivity(new Intent(getApplicationContext(), ListaProyectosActivity.class));
             }
         });
-        btVerProyectosUsuario=findViewById(R.id.btVerProyectosUsuario);
+        btVerProyectosUsuario = findViewById(R.id.btVerProyectosUsuario);
         btVerProyectosUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,42 +95,37 @@ public class ProyectoActivity extends AppCompatActivity {
         });
 
     }
+
     //Crear Proyectos
-    public void crearProyecto(){
-        Proyecto proyecto=new Proyecto();
+    public void crearProyecto() {
+        Proyecto proyecto = new Proyecto();
 
         proyecto.setNombre(etProyecto.getText().toString().toUpperCase().trim());
-        if(etProyecto.getText().toString().isEmpty()){
+        if (etProyecto.getText().toString().isEmpty()) {
             etProyecto.setError("Ingrese el nombre del proyecto");
             etProyecto.requestFocus();
             return;
         }
         proyecto.setFecha_inicio(etFechaInicio.getText().toString().trim());
-        if(etFechaInicio.getText().toString().isEmpty()){
+        if (etFechaInicio.getText().toString().isEmpty()) {
             etFechaInicio.setError("Ingrese una fecha de inicio");
             etFechaInicio.requestFocus();
             return;
         }
-        proyecto.setFecha_fin(etFechaFin.getText().toString().trim());
-        if(etFechaFin.getText().toString().isEmpty()){
-            etFechaFin.setError("Ingrese una fecha de fin");
-            etFechaFin.requestFocus();
-            return;
-        }
         proyecto.setDistrito(etDistrito.getText().toString().trim().toUpperCase());
-        if(etDistrito.getText().toString().isEmpty()){
+        if (etDistrito.getText().toString().isEmpty()) {
             etDistrito.setError("Ingrese distrito");
             etDistrito.requestFocus();
             return;
         }
         proyecto.setProvincia(etProvincia.getText().toString().trim().toUpperCase());
-        if(etProvincia.getText().toString().isEmpty()){
+        if (etProvincia.getText().toString().isEmpty()) {
             etProvincia.setError("Ingrese provincia");
             etProvincia.requestFocus();
             return;
         }
         proyecto.setDepartamento(etDepartamento.getText().toString().trim().toUpperCase());
-        if(etDepartamento.getText().toString().isEmpty()){
+        if (etDepartamento.getText().toString().isEmpty()) {
             etDepartamento.setError("Ingrese departamento");
             etDepartamento.requestFocus();
             return;
@@ -158,9 +138,9 @@ public class ProyectoActivity extends AppCompatActivity {
         call.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
-                if(response.code()==201){
+                if (response.code() == 201) {
                     Log.d("TAG1", "Proyecto creado correctamente");
-                    startActivity(new Intent(getApplicationContext(),ListaProyectosActivity.class));
+                    startActivity(new Intent(getApplicationContext(), ListaProyectosActivity.class));
                     finish();
 
                 }
@@ -172,8 +152,9 @@ public class ProyectoActivity extends AppCompatActivity {
             }
         });
     }
+
     //Ver todos los Proyectos
-    public void verTodosLosProyectos(){
+    public void verTodosLosProyectos() {
 
         Call<List<Proyecto>> call = WebService
                 .getInstance()
@@ -183,12 +164,12 @@ public class ProyectoActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Proyecto>>() {
             @Override
             public void onResponse(Call<List<Proyecto>> call, Response<List<Proyecto>> response) {
-                if(response.code()==200){
-                    for(int i=0; i<response.body().size(); i++){
+                if (response.code() == 200) {
+                    for (int i = 0; i < response.body().size(); i++) {
                         Log.d("TAG1", "Nombre Proyecto: " + response.body().get(i).getNombre()
                                 + "Codigo Proyecto: " + response.body().get(i).getProusuario());
                     }
-                }else if(response.code()==404){
+                } else if (response.code() == 404) {
                     Log.d("TAG1", "No hay proyectos");
                     Toast.makeText(ProyectoActivity.this, "No hay proyectos", Toast.LENGTH_SHORT).show();
                 }
@@ -204,7 +185,7 @@ public class ProyectoActivity extends AppCompatActivity {
     }
 
     //Ver proyectos por Usuario
-    public void verProyectosPorUsuario(){
+    public void verProyectosPorUsuario() {
         Call<List<Proyecto>> call = WebService
                 .getInstance()
                 .createService(WebServiceApi.class)
@@ -212,16 +193,17 @@ public class ProyectoActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Proyecto>>() {
             @Override
             public void onResponse(Call<List<Proyecto>> call, Response<List<Proyecto>> response) {
-                    if(response.code()==200){
-                        for(int i=0; i<response.body().size(); i++){
-                            Log.d("TAG1", "Nombre Proyecto: " + response.body().get(i).getNombre()
-                                    + "  Codigo Usuario: " + response.body().get(i).getProusuario());
-                        }
-                    }else if(response.code()==404){
+                if (response.code() == 200) {
+                    for (int i = 0; i < response.body().size(); i++) {
+                        Log.d("TAG1", "Nombre Proyecto: " + response.body().get(i).getNombre()
+                                + "  Codigo Usuario: " + response.body().get(i).getProusuario());
+                    }
+                } else if (response.code() == 404) {
                     Log.d("TAG1", "No hay proyectos");
                     Toast.makeText(ProyectoActivity.this, "No hay proyectos", Toast.LENGTH_SHORT).show();
                 }
             }
+
             @Override
             public void onFailure(Call<List<Proyecto>> call, Throwable t) {
 
@@ -229,11 +211,11 @@ public class ProyectoActivity extends AppCompatActivity {
         });
 
     }
+
     //DatePicker de Proyectos//
 
     private void colocar_fecha() {
-        etFechaInicio.setText (mDayIni + "/" + (mMonthIni + 1) + "/" + mYearIni+" ");
-        etFechaFin.setText(mDayIni + "/" + (mMonthIni + 1) + "/" + mYearIni+" ");
+        etFechaInicio.setText(mDayIni + "/" + (mMonthIni + 1) + "/" + mYearIni + " ");
     }
 
     private DatePickerDialog.OnDateSetListener mDateSetListener =
@@ -243,10 +225,10 @@ public class ProyectoActivity extends AppCompatActivity {
                     mMonthIni = monthOfYear;
                     mDayIni = dayOfMonth;
                     colocar_fecha();
-
                 }
 
             };
+
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
@@ -254,5 +236,8 @@ public class ProyectoActivity extends AppCompatActivity {
                 return new DatePickerDialog(this, mDateSetListener, sYearIni, sMonthIni, sDayIni);
         }
         return null;
+
+
     }
+
 }
